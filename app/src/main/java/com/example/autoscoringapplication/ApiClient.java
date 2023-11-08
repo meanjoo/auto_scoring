f8dc007c5e8a09f292ba096fe9479a1c5e6ace3b
meanjoo <meanjooo@naver.com>
2023-11-08 09:34:13 +0900

    set http timeout

--- app/src/main/java/com/example/autoscoringapplication/ApiClient.java
+++ app/src/main/java/com/example/autoscoringapplication/ApiClient.java
@@ -3,12 +3,14 @@ package com.example.autoscoringapplication;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASEURL = "http://127.0.0.1:5000/";
-
+    private static final String BASEURL = "http://127.0.0.1:5000";
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofit() {
@@ -16,8 +18,16 @@ public class ApiClient {
             Gson gson = new GsonBuilder()
                     .setLenient()
                     .create();
+
+            OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                    .connectTimeout(300, TimeUnit.SECONDS)
+                    .readTimeout(300, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASEURL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
